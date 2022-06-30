76a4fbe908a5960ac4c73a9c718c2f469e777f6b
Zuwairanajma <juwairiyyasadiq@gmail.com>
2022-06-30 14:36:42 -0700

    another trial

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 #include <unistd.h>
-
 /**
-* main -prints exactly "and that piece of art is useful" 
-* -Dora Korpar, 2015-10-19", followed by a new line.
-* Return: Always 0 (success)
+*main-prints exactly "and that piece of art is useful"
+*- Dora Korpar, 2015-10-19".
+*followed by a new line, to the standard error.
+*Return: Always 0 (success)
 */
 int main(void)
 {
-write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-Return(1);
+write(2, "and that piece of art is useful\"-Dora Korpar, 2015-10-19\n", 59);
+return (1);
 }
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,20 +1,20 @@
 #include <stdio.h>
 /**
- * main - Entry point
+ * main - prints the size of various types
  * Return: Always 0 (success)
  */
 int main(void)
 {
-int i;
-long int l;
-long long int ll
+int a;
+long int b;
+long long int c;
 float d;
 char c;
 
 printf("Size of a char: %lu byte(s)\n", sizeof(c));
-printf("Size of an int: %lu byte(s)\n", sizeof(i));
-printf("Size of a long int: %lu byte(s)\n", sizeof(l));
-printf("Size of a long long int: %lu byte(s)\n", sizeof(ll));
+printf("Size of an int: %lu byte(s)\n", sizeof(a));
+printf("Size of a long int: %lu byte(s)\n", sizeof(b));
+printf("Size of a long long int: %lu byte(s)\n", sizeof(c));
 printf("Size of a float: %lu byte(s)\n", sizeof(d));
 return (0);
 }
