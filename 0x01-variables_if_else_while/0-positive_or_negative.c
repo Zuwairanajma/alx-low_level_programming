5403fd5515c8ba51aa5dbdb7db6a6b24a89ca03a
Zuwairanajma <juwairiyyasadiq@gmail.com>
2022-07-03 02:29:56 -0700

    yet another first set trial

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,23 +3,22 @@
 #include <stdio.h>
 
 /**
-* main -> assigns a random number to the variable n each time it is executed.
-* and base a condition
-* Return : always 0
-*/
+ * main - assigns a random number to int n everytime
+ * it executes, and prints it
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-int n; "completes the source code"
+	int n;
 
-srand(time(0));
-n = rand() - RAND_MAX / 2;
-if (n > 0)
-	printf("%d is positive\n", n);
-if (n == 0)
-	printf("%d is zero\n", n);
-if (n < 0)
-{
-	printf("%d is negative\n", n);
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n > 0)
+		printf("%d is positive\n", n);
+	else if (n == 0)
+		printf("%d is zero\n", n);
+	else if (n < 0)
+		printf("%d is negative\n", n);
+	return (0);
 }
-return (0);
-}
+
--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -3,31 +3,24 @@
 #include <stdio.h>
 
 /**
-* main ->  assign a random number to the variable n each time it is executed.
-* print the last digit of the number stored in the variable n.
-Return: Always 0 (success)
-*/
+ * main - prints the last digit of the random
+ * number stored in the variable n
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-write("the last digit of the number stored in the variable n")
-int n;
+	int n;
 
-srand(time(0));
-n = rand() - RAND_MAX / 2;
-printf("Last digit of %d is ", n);
-if (n > 5)
-{
-	prinf("greater than 5");
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	printf("Last digit of %d is %d ", n, n % 10);
+	if (n % 10 > 5)
+		printf("and is greater than 5\n");
+	else if (n % 10 == 0)
+		printf("and is 0\n");
+	else if (n % 10 < 6 && n % 10 != 0)
+		printf("and is less than 6 and not 0\n");
+	return (0);
 }
-if (n == 0)
-{
-	printf("0")
-}
-if (n < 6 && n != 0)
-{
-	printf("less than 6 and not 0")
-}
-printf("\n")
 
-return (0);
-}
+
