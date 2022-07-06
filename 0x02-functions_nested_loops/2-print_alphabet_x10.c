3bee5bda37a2cbbfcaa3a1d008c923973d2ad3f3
Zuwairanajma <juwairiyyasadiq@gmail.com>
2022-07-05 22:34:34 -0700

    2prints

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -9,13 +9,13 @@
 void print_alphabet_x10(void)
 {
 	char c, i;
-: wq
+
 	for (i = 0; i <= 9; i++)
 	{
 		for (c = 'a'; c <= 'z'; c++)
 		{
 			_putchar(c);
 		}
-		_putchar('\n'); :
+		_putchar('\n');
 	}
 }
