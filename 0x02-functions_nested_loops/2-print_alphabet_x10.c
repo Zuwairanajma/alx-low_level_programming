d60589a1a94dbe66c8167d32b2f7bccb2b7c2c16
Zuwairanajma <juwairiyyasadiq@gmail.com>
2022-07-05 22:26:23 -0700

    2print

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,9 +1,11 @@
 #include "main.h"
+
 /**
  * print_alphabet_x10 - function to print abc 10 times
  *
  * Return: 0
  */
+
 void print_alphabet_x10(void)
 {
 	char c, i;
@@ -17,4 +19,3 @@ void print_alphabet_x10(void)
 		_putchar('\n'); :
 	}
 }
-
