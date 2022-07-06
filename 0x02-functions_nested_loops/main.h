b8a794812d8a7318d841ea9d4109428a56d0716b
Zuwairanajma <juwairiyyasadiq@gmail.com>
2022-07-05 22:19:31 -0700

    main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,3 +1,6 @@
+#ifndef MAIN_H
+#define MAIN_H
+
 int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
@@ -11,4 +14,6 @@ void times_table(void);
 int add(int a, int b);
 void print_to_98(int n);
 void print_times_table(int n);
-void num(int r, int n)
+void num(int r, int n);
+
+#endif
