9edc2ecbaf24d384a2b04bb5300effbba5eae25f
Zuwairanajma <juwairiyyasadiq@gmail.com>
2022-07-22 21:54:34 -0700

    second trial

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -5,7 +5,7 @@
  * @argv: array that contains the program command line arguments.
  * Return: 0 - success.
  */
-int main(int argc, char *argv[] _attribute_((unused)))
+int main(int argc, char *argv[] __attribute__((unused)))
 {
 	printf("%d\n", argc - 1);
 	return (0);
--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -1,4 +1,4 @@
-#ifndef MAIN_H
+#ifndef MAAIN_H
 #define MAIN_H
 
 int _putchar(char c);
